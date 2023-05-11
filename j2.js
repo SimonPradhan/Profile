3848efdb465fc1618a6d65e5acce691ed940568c
Simon Pradhan <np03cs4s220192@heraldcollege.edu.np>
2023-05-11 16:31:42 +0545

    initial commit

---
+++ j2.js
@@ -0,0 +1,4 @@
+button = document.getElementById('but');
+button.addEventListener('click', () => {
+    alert("You Stupid Fool")
+})
---
+++ script.js
@@ -0,0 +1,1 @@
+document.querySelector(".head_one").innerHTML= "Kristal";
